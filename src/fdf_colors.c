e35049fe369377388218c2289107451974e1a0ad
Cristian Ayuso <crayfe@gmail.com>
2025-04-09 12:20:12 +0200

    Update fdf_colors.c

--- src/fdf_colors.c
+++ src/fdf_colors.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   fdf_colors.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crayfe <crayfe@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cayuso-f <causo-f@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:44:08 by cayuso-f          #+#    #+#             */
 /*   Updated: 2025/04/03 20:00:26 by crayfe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "FdF.h"
+#include "fdf.h"
 
 int	read_color(char *data)
 {
